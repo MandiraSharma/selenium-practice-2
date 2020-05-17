701893961dda45e1f85410a61541a20aacfefa19
mandiosho <60794614+mandiosho@users.noreply.github.com>
2020-05-17 14:29:33 -0500

    commit1

--- src/main/java/assignment/HomeWork3.java
+++ src/main/java/assignment/HomeWork3.java
@@ -1,5 +1,17 @@
 package assignment;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HomeWork3 {
+	WebDriver driver;
+	public void LaunchBrowsers() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Mandira\\selenium\\SeleniumSession1\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://techfios.com/test/billing/?ng=admin/");
+		driver.manage().window().maximize();
+
+	}
 
 }
